f65b37989daa421ca497531033c71172433bbdfe
Npf <8747375-naipofo@users.noreply.gitlab.com>
2022-02-17 08:05:02 +0100

    rebranding once more

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,7 @@ attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap
 }).addTo(map);
 L.control.scale({imperial: false, metric: true}).addTo(map);
 L.marker({lon: 17.05538, lat: 51.12580}).bindPopup('Shape of Web').addTo(map);
+
+document.querySelector("nav").addEventListener("click", ()=>document.querySelectorAll("li>a").forEach(e=>setTimeout(()=>e.classList[e.href == document.location ? "add" : "remove"]("current-page"),20)))
+
+document.querySelector("nav").click()
